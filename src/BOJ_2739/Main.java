0ee526fcc8dce8e06efa403f79a84ed12f3a2de6
Amepistheo <kellylhji@gmail.com>
2023-07-03 15:41:49 +0900

    백준 2739번 : 구구단

--- src/BOJ_2739/Main.java
+++ src/BOJ_2739/Main.java
@@ -10,7 +10,7 @@ public class Main {
         n = sc.nextInt();
 
         for (int i=1; i<10; i++) {
-            System.out.println(n + " * " + i + " = " + n*i);
+            System.out.println(n + " * " + i + " = " + (n*i));
         }
     }
 }
