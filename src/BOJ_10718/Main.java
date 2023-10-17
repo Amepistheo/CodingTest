e19c9894de237a8e2f130affdcaf67534f2c2da8
Amepistheo <kellylhji@gmail.com>
2023-10-17 13:29:13 +0900

    백준 10718번 : We love kriii

---
+++ src/BOJ_10718/Main.java
@@ -0,0 +1,18 @@
+// 10718번 We love kriii
+package BOJ_10718;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0; i<2; i++) {
+            sb.append("강한친구").append(" ").append("대한육군").append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
