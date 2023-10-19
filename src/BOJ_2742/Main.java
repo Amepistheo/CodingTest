cb96acb83e19ce32c87cdf6a22d966e3e082f14a
Amepistheo <kellylhji@gmail.com>
2023-10-19 11:20:43 +0900

    백준 2742번 : 기찍 N

---
+++ src/BOJ_2742/Main.java
@@ -0,0 +1,20 @@
+// 2742번 기찍 N
+package BOJ_2742;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=N; i>0; i--) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
