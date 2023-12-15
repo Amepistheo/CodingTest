32f2d6fbc55d78055b48c6ef95d7bf9eac3d4aac
Amepistheo <kellylhji@gmail.com>
2023-12-15 14:56:59 +0900

    백준 5543번 : 상근날드

---
+++ src/BOJ_5543/Main.java
@@ -0,0 +1,27 @@
+// 5543번 상근날드
+package BOJ_5543;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] burger = new int[3];
+        int[] drink = new int[2];
+
+        for (int i=0; i<3; i++) {
+            burger[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i=0; i<2; i++) {
+            drink[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(burger);
+        Arrays.sort(drink);
+
+        System.out.println(burger[0] + drink[0] - 50);
+    }
+}
