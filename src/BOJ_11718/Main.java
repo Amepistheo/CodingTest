7589f8efc05f3c7732030b13ac1698a00f986656
Amepistheo <kellylhji@gmail.com>
2024-01-05 13:39:04 +0900

    백준 11718번 : 그대로 출력하기

---
+++ src/BOJ_11718/Main.java
@@ -0,0 +1,17 @@
+// 11718번 그대로 출력하기
+package BOJ_11718;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+}
