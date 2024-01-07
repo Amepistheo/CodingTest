fc6f7c88622a35eb9ace5e2c014e1cb128fe3611
Amepistheo <kellylhji@gmail.com>
2024-01-07 16:27:29 +0900

    백준 1449번 : 수리공 항승

---
+++ src/BOJ_1449/Main.java
@@ -0,0 +1,39 @@
+// 1449번 수리공 항승
+package BOJ_1449;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int[] water = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            water[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(water);
+
+        double left = water[0] - 0.5;
+        int count = 1;
+
+        for (int i = 0; i < N; i++) {
+            if (left + L < water[i]) {
+                count++;
+                left = water[i] - 0.5;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
