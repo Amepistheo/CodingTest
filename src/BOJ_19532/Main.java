66da9088887e5b41662b9db35e960c6aae6bd3c6
Amepistheo <kellylhji@gmail.com>
2024-01-29 13:13:41 +0900

    백준 19532번 : 수학은 비대면강의입니다

---
+++ src/BOJ_19532/Main.java
@@ -0,0 +1,33 @@
+// 19532번 수학은 비대면강의입니다
+package BOJ_19532;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        for (int i = -999; i <= 999; i++) {
+            for (int j = -999; j <= 999; j++) {
+                if ((a * i) + (b * j) == c && (d * i) + (e * j) == f) {
+                    sb.append(i).append(" ").append(j);
+                    break;
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
