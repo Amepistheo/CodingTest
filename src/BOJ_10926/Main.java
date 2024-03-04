8f5cfeb1b512d5c05443f63c167432f6c781dae1
Amepistheo <kellylhji@gmail.com>
2024-03-04 18:19:32 +0900

    백준 10926번 : ??!

---
+++ src/BOJ_10926/Main.java
@@ -0,0 +1,15 @@
+// 10926번 ??!
+package BOJ_10926;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        System.out.println(input + "??!");
+    }
+}
