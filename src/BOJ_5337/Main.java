c4676145162c823038ad5fdeba615dd626125a48
Amepistheo <kellylhji@gmail.com>
2024-03-24 12:02:29 +0900

    백준 5337번 : 웰컴

---
+++ src/BOJ_5337/Main.java
@@ -0,0 +1,18 @@
+// 5337번 웰컴
+package BOJ_5337;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(".  .   .").append("\n");
+        sb.append("|  | _ | _. _ ._ _  _").append("\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.").append("\n");
+
+        System.out.print(sb);
+    }
+}
