c08611c653c8fb7bb211f319a04728b2c2f7dd13
Amepistheo <kellylhji@gmail.com>
2024-06-04 23:53:23 +0900

    백준 2743번 : 단어 길이 재기

---
+++ src/BOJ_2743/Main.java
@@ -0,0 +1,15 @@
+// 2743번 단어 길이 재기
+package BOJ_2743;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+
+        System.out.println(word.length());
+    }
+}
