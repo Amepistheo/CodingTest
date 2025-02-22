9f375c58d2b84209a0012ff8ec180b7b7fe0710d
Amepistheo <kellylhji@gmail.com>
2025-02-22 21:05:01 +0900

    백준 2294번 : 동전 2

---
+++ src/BOJ_2294/Main.java
@@ -0,0 +1,37 @@
+// 2294번 동전 2
+package BOJ_2294;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[n];
+        int[] dp = new int[k + 1];
+
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
+            }
+        }
+
+        int result = dp[k] == Integer.MAX_VALUE - 1 ? -1 : dp[k];
+
+        System.out.println(result);
+    }
+}
