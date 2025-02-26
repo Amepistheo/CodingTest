d1c085ba487a8f9b8b6cc02961ab91cce396f00a
Amepistheo <kellylhji@gmail.com>
2025-02-26 13:33:06 +0900

    백준 2225번 : 합분해

---
+++ src/BOJ_2225/Main.java
@@ -0,0 +1,36 @@
+// 2225번 합분해
+package BOJ_2225;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 0; i <= N; i++) {
+            dp[i][0] = 0;
+            dp[i][1] = 1;
+        }
+
+        for (int i = 0; i <= K; i++) {
+            dp[1][i] = i;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 2; j <= K; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
