0a0505ac169e20cc9a01b577a53dd30fffb878f0
Amepistheo <kellylhji@gmail.com>
2025-04-04 15:29:27 +0900

    백준 20040번 : 사이클 계산

---
+++ src/BOJ_20040/Main.java
@@ -0,0 +1,58 @@
+// 20040번 사이클 게임
+package BOJ_20040;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n];
+        int num = 0;
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (find(a) == find(b)) {
+                num = i;
+                break;
+            }
+
+            union(a, b);
+        }
+
+        System.out.println(num);
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        parent[rootY] = rootX;
+    }
+}
