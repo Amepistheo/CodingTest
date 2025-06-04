024d761aaf44f3ae0e5738a672f0554c064c04dd
Amepistheo <kellylhji@gmail.com>
2025-06-04 22:51:04 +0900

    프로그래머스 1 : 음양 더하기

---
+++ src/PG_76501/Solution.java
@@ -0,0 +1,18 @@
+// 76501번 음양 더하기
+package PG_76501;
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
+}
